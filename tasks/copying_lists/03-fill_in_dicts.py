b9fc789870fc9880b135c4fbbd4b63ec4f0a8d17
Rich107 <small.car3251@fastmail.com>
2025-07-07 09:10:03 +0100

    feat: basic list replace example

---
+++ tasks/copying_lists/03-fill_in_dicts.py
@@ -0,0 +1,31 @@
+"""
+switch the values of the keys in the dictionary
+You could do this using multi cursors.
+
+"""
+
+wrong_list = {
+    "red": "apple",
+    "yellow": "banana",
+    "purple": "grape",
+    "orange": "orange",
+    "brown": "kiwi",
+    "yellow": "mango",
+    "pink": "peach",
+    "green": "pear",
+    "purple": "plum",
+    "red": "strawberry",
+}
+
+right_list = {
+    "apple": "red",
+    "banana": "yellow",
+    "grape": "purple",
+    "orange": "orange",
+    "kiwi": "brown",
+    "mango": "yellow",
+    "peach": "pink",
+    "pear": "green",
+    "plum": "purple",
+    "strawberry": "red",
+}
