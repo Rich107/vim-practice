df6740feae3c3c8024f7ce4c5c45a9dd57468327
Rich107 <small.car3251@fastmail.com>
2025-07-09 13:04:58 +0100

    keys only file

---
+++ tasks/copying_lists/03b-fill_in_dicts.py
@@ -0,0 +1,16 @@
+"""
+Take these keys and put them into file 3a-fill_in_dicts.py
+"""
+
+keys = [
+    "red",
+    "yellow",
+    "purple",
+    "orange",
+    "brown",
+    "yellow",
+    "pink",
+    "green",
+    "purple",
+    "red",
+]
