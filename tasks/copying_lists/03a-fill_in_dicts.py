ac6dac668bcecce91b5498cc20548267a220e7bd
Rich107 <small.car3251@fastmail.com>
2025-07-09 07:32:13 +0100

    feat: added follow up activity for activity 3

---
+++ tasks/copying_lists/03a-fill_in_dicts.py
@@ -0,0 +1,29 @@
+"""
+Take the keys from the last file and paste them into the missing keys in this file
+"""
+
+incomplete_list = {
+    "": "apple",
+    "": "banana",
+    "": "grape",
+    "": "orange",
+    "": "kiwi",
+    "": "mango",
+    "": "peach",
+    "": "pear",
+    "": "plum",
+    "": "strawberry",
+}
+
+incomplete_list = {
+    "red": "apple",
+    "yellow": "banana",
+    "purple": "grape",
+    "orange": "orange",
+    "brown": "kiwi",
+    "yellow": "mango",
+    "pink": "peach",
+    "green": "pear",
+    "purple": "plum",
+    "red": "strawberry",
+}
