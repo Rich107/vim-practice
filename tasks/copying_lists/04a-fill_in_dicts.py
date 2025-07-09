3d71321adb974ed42c8e4340e90a85a2917ae4a7
Rich107 <small.car3251@fastmail.com>
2025-07-09 18:08:23 +0100

    feat: challenge 4 created

---
+++ tasks/copying_lists/04a-fill_in_dicts.py
@@ -0,0 +1,29 @@
+"""
+Take the keys from file 3b and paste them into the missing keys in this file
+"""
+
+incomplete_list = {
+    "red": "",
+    "yellow box": "",
+    "purple": "",
+    "round orange": "",
+    "brown": "",
+    "yellow": "",
+    "pink": "",
+    "green": "",
+    "purple": "",
+    "red": "",
+}
+
+complete_list = {
+    "red": "apple",
+    "yellow box": "banana",
+    "purple": "grape",
+    "round orange": "orange",
+    "brown": "kiwi",
+    "yellow": "mango",
+    "pink": "peach",
+    "green": "pear",
+    "purple": "plum",
+    "red": "strawberry",
+}
---
+++ tasks/copying_lists/04b-fill_in_dicts.py
@@ -0,0 +1,16 @@
+"""
+Take these keys and put them into file 4a-fill_in_dicts.py
+"""
+
+keys = [
+    "apple",
+    "banana",
+    "grape",
+    "orange",
+    "kiwi",
+    "mango",
+    "peach",
+    "pear",
+    "plum",
+    "strawberry",
+]
