14883a7912d50b09653771863945901b9e711286
Rich107 <small.car3251@fastmail.com>
2025-07-12 22:19:38 +0100

    feat: adding feature flags

---
+++ tasks/copying_lists/changing_case/01-screaming-flags.py
@@ -0,0 +1,12 @@
+"""
+Feature flags are a common way to control the behavior of a program.
+adding new ones usually means adding one to the end of a list and updating the case.
+What is the fastest way to add items to this list of flags?
+You need to add five new flags to the list.
+You may not solve this by copying and pasting values from another file.
+"""
+
+
+class Flags:
+    THIS_NEW_FEATURE_IS_AWESOME = "this_new_feature_is_awesome"
+    BUT_WHY_ALL_THE_SHOUTING = "but_why_all_the_shouting"
